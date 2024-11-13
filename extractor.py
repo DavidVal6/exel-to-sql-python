a465107e2fef5ad8d7caa570ca9b1e8f267ed264
DavidVal6 <98176834+DavidVal6@users.noreply.github.com>
2024-11-12 23:27:00 -0500

    Feature IDS_list

--- extractor.py
+++ extractor.py
@@ -2,8 +2,16 @@ import pandas as pd
 
 excel_archive = 'datos.xlsx'
 exit_sql = "script.sql"
+exit_list = "listed.sql"
 
 extraction = pd.read_excel(excel_archive, sheet_name='Bloqueo')
+
+
+sql_list="""
+UPDATE TABLE
+SET ESTADO = BLOQUEADO
+WHERE ID IN ({IDS})
+"""
 
 sql_template_forbloked = """
 SELECT *
@@ -16,14 +24,23 @@ SET ESTADO = DESBLOQUEADO
 WHERE ID = {id};
 """
 
+ids = []
 sql_sentences = []
 for _, row in extraction.iterrows():
     elemnt_id = row['ID']
     if row['VALUES'] == 'BLOQUEAR':
         sentence = sql_template_forbloked.format(id=elemnt_id)
+        ids.append(elemnt_id)
     else:
         sentence = sql_template_forunbloked.format(id = elemnt_id)
     sql_sentences.append(sentence)
+    
+ids_str = ", ".join(map(str, ids))
+sentence_list = sql_list.format(IDS=ids_str)
+
+with open(exit_list, 'w') as file:
+    file.write(sentence_list)
+print('Script Generado de manera exitosa')
 with open(exit_sql, "w") as file:
     file.write("\n".join(sql_sentences))
 print('Script Generado de manera exitosa')
